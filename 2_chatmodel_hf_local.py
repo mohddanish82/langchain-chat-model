852cc13220f3e1f42fa1ff1f52aa433d7dd76364
Danish <mohddanish15635@gmail.com>
2026-08-03 10:16:50 +0530

    Refactor chatbot code and fix class naming

--- 2_chatmodel_hf_local.py
+++ 2_chatmodel_hf_local.py
@@ -1,35 +1,46 @@
-from langchain_huggingface import ChatHuggingFace, HuggingFacePipeline
-import os
 from fastapi import FastAPI
 from pydantic import BaseModel
+from langchain_huggingface import ChatHuggingFace, HuggingFacePipeline
+import os
 
+# Create FastAPI App
 app = FastAPI()
+
+# Home Endpoint
 @app.get("/")
 def home():
     return {
-    "message": "This is a simple chatbot using LangChain and Hugging Face",
-    "title": "Implemented by MOHD DANISH"
+        "message": "This is a simple chatbot using LangChain and Hugging Face",
+        "title": "Implemented by MOHD DANISH"
     }
-os.environ['HF_HOME'] = 'D:/huggingface_cache'
+
+# Hugging Face Cache
+os.environ["HF_HOME"] = "D:/huggingface_cache"
+
+# Load Model
 llm = HuggingFacePipeline.from_model_id(
-    model_id='TinyLlama/TinyLlama-1.1B-Chat-v1.0',
-    task='text-generation',
-    pipeline_kwargs=dict(
-        temperature=0.5,
-        max_new_tokens=100
-    )
+    model_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+    task="text-generation",
+    pipeline_kwargs={
+        "temperature": 0.5,
+        "max_new_tokens": 100
+    }
 )
 
+# Create Chat Model
 model = ChatHuggingFace(llm=llm)
 
-class Simplechat(BaseModel):
+# Request Body
+class SimpleChat(BaseModel):
     question: str
 
-
+# Chat Endpoint
 @app.post("/simplechat")
-def chat_model(data: Simplechat):
-    respone = model.invoke(data.question)
-    return{
+def chat_model(data: SimpleChat):
+
+    response = model.invoke(data.question)
+
+    return {
         "question": data.question,
         "answer": response.content
     }
